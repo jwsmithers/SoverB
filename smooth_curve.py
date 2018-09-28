4fd456e48cbe5283e320d0a9f1fac7e86b879991
jwsmith <joshua.wyatt.smith@cern.ch>
2018-09-29 01:32:47 +0200

    add uncerts

--- smooth_curve.py
+++ smooth_curve.py
@@ -8,39 +8,75 @@ import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
 set_style('ATLAS', mpl=True)
 
+#from matplotlib import rc
+#rc('text', usetex=True)
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 
-total_error_with_ppt=[61.851959,
-73.557281,
-77.241753,
-73.537308,
-52.451118,
-77.259621,
-62.263344,
-66.908913,
-66.775131,
-69.740173,
-49.274830,
-54.018188,
-77.873222,
-92.991486,
-165.950577]
+signal_error_with_ppt = [27.190201, 
+31.898310,
+52.579323,
+36.546188,
+32.053516,
+43.168060,
+47.035297,
+54.571823,
+55.644463,
+50.347736,
+33.956764,
+43.365265,
+57.821518,
+77.889450,
+156.318161]
 
-total_error_without_ppt=[38.422432,
-47.862087,
-43.504967,
-56.230820,
-79.577484,
-43.515686,
-59.012699,
-77.111404,
-62.119514,
-60.963024,
-76.491035,
-146.361496,
-118.906387,
-60.736725,
-60.924484]
+background_error_with_ppt = [59.428699, 
+68.674637,
+74.488487,
+65.303169,
+46.686226,
+66.499092,
+49.610222,
+40.999603,
+42.809963,
+51.230968,
+39.774769,
+37.171600,
+50.436943,
+43.418961,
+46.070732]
 
+
+signal_error_without_ppt=[19.091253, 
+26.131187,
+21.160421,
+24.735683,
+48.555183,
+31.894642,
+35.454357,
+58.091515,
+45.814583,
+52.198219,
+51.320858,
+96.067398,
+69.173141,
+51.244789,
+59.280148]
+
+
+background_error_without_ppt=[37.967445, 
+45.762615,
+41.780563,
+54.296570,
+77.235046,
+35.616005,
+52.660904,
+58.515697,
+45.664909,
+37.177460,
+60.152164,
+111.847214,
+97.058266,
+28.016979,
+14.711366]
 
 def return_errors(hist):
   error = []
@@ -48,14 +84,9 @@ def return_errors(hist):
     error.append(hist.GetBinError(i))
   return error
 
-def return_errors_graph(hist):
-  error = []
-  for i in range(0,15):
-    error.append(hist.GetErrorY(i))
-  return error
-
 def square(L):
-    return np.asarray([i ** 2 for i in L])
+    #return np.asarray([i ** 2 for i in L])
+    return [i ** 2 for i in L]
 
 def square_root(L):
     return np.asarray([math.sqrt(i) for i in L])
@@ -64,23 +95,6 @@ def square_root(L):
 def retreive_events(path,File):
 
   filename = path+File
-
-#  f_postfit = ROOT.TFile.Open(path+post_fit_filename)
-#  ttphoton_hist = f.Get("h_ttphoton_postFit")
-#  ttphoton_hist_errors = return_errors(ttphoton_hist)
-#  hfake_hist = f.Get("h_hadronfakes_postFit")
-#  hfake_hist_errors = return_errors(hfake_hist)
-#  efake_hist = f.Get("h_electronfakes_postFit")
-#  efake_hist_errors = return_errors(efake_hist)
-#  qcd_hist = f.Get("h_QCD_postFit")
-#  qcd_hist_errors = return_errors(qcd_hist)
-#  wphoton_hist = f.Get("h_Wphoton_postFit")
-#  wphoton_hist_errors = return_errors(wphoton_hist)
-#  other_hist = f.Get("h_Other_postFit")
-#  other_hist_errors=return_errors(other_hist)
-
-#  _total_error = f_postfit.Get("g_totErr_postFit")
-#  total_errors = return_errors_graph(_total_error)
 
   #prefit
   f = ROOT.TFile.Open(filename)
@@ -113,10 +127,6 @@ def retreive_events(path,File):
   significance_errors = []
 
 
-  #add_bkg_errors = square(hfake_hist_errors)+square(efake_hist_errors)+square(qcd_hist_errors)+square(wphoton_hist_errors)+square(other_hist_errors)
-  #total_bkg_error = square_root(add_bkg_errors)
-
-
   #Now we cut at each bin and calculate the total s/b
   for i in range(0,len(signal_hist)):
     s = sum(signal_hist[i:])
@@ -124,20 +134,24 @@ def retreive_events(path,File):
     s_over_b.append(s/b)
     significance.append(s/math.sqrt(b))
 
-#    #calculate the error:
-#    ds = ttphoton_hist_errors[i]
-#    db = total_bkg_error[i]
-#    s_over_b_error.append(float(math.sqrt( ((b**2*ds**2)+(db**2*s**2))/b**4 )))
-#    significance_errors.append((1/2.)*math.sqrt((4*(b**2)*(ds**2) + (db**2)*(s**2))/b**3))
+    #if "NO_PPT" in path:
+    #  s_err = signal_error_without_ppt[i]
+    #  b_err = background_error_without_ppt[i]
+    #else:
+    #  s_err = signal_error_with_ppt[i]
+    #  b_err = background_error_with_ppt[i]
 
     if "NO_PPT" in path:
-      total_error_list = total_error_without_ppt[i]
+      s_err = math.sqrt(sum(square(signal_error_without_ppt[i:])))
+      b_err = math.sqrt(sum(square(background_error_without_ppt[i:])))
     else:
-      total_error_list = total_error_with_ppt[i]
+      s_err = math.sqrt(sum(square(signal_error_with_ppt[i:])))
+      b_err = math.sqrt(sum(square(background_error_with_ppt[i:])))
 
-    # Scale the total error respectively
-    s_over_b_error.append((total_error_list*(s/b))/(s+b))
-    significance_errors.append((total_error_list*(s/math.sqrt(b)))/(s+b))
+    s_over_b_error.append(math.sqrt(((s**2*b_err**2)+(s_err**2*b**2))/b**4))
+    num = s**2*b_err**2+4*s_err**2*b**2
+    denom = b**3
+    significance_errors.append(0.5*math.sqrt(num/denom))
 
   return s_over_b, significance, np.asarray(s_over_b_error), np.asarray(significance_errors)
 
@@ -194,11 +208,14 @@ ax2.fill_between(my_xticks, significance_withoutppt-significance_withoutppt_erro
 
 withppt = mlines.Line2D([], [], color='black',linewidth=2, label=r'with PPT')
 withoutppt = mlines.Line2D([], [], color='black',linewidth=2, label=r'without PPT', linestyle="--")
-uncert = mpatches.Patch(color='grey', label='Systematic uncertainty')
+uncert = mpatches.Patch(color='grey', label=r'Stat. $\oplus$ Syst.')
 
 
-
-ax1.text(0.18,3.7,r"$\sqrt{s}=$13 TeV, 36.1 fb$^{-1}$",
+ax1.text(0.18,3.8,r"ATLAS",style='italic',weight='bold',
+          fontsize=18, color='black')
+ax1.text(0.38,3.8,r"Internal",
+          fontsize=18, color='black')
+ax1.text(0.18,3.4,r"$\sqrt{s}=$13 TeV, 36.1 fb$^{-1}$",
           fontsize=14, color='black')
 plt.legend(handles=[withppt,withoutppt,uncert],loc=(0.2,0.45),frameon=False,fontsize =13)
 
